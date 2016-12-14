d14adfe2b5686a7968a58e7c1f1dcbcc07f2c03f
blanche <blanche@users.noreply.github.com>
2016-12-14 09:54:38 +0000

    remove temporary debug variable from connection.py

--- src/db/connection.py
+++ src/db/connection.py
@@ -2,15 +2,13 @@ from pymongo import MongoClient
 
 
 class DbConnection:
-    host = "localhost"
-    port = 27017
-    auth = False
-    # host = "svm-lw4u16-comp6235-group-6.ecs.soton.ac.uk"
-    # port = 27018
-    # auth = True
-    # db = "restaurants"
-    db = "yelp4"
-    # db = "ham-nor"
+    # host = "localhost"
+    # port = 27017
+    # auth = False
+    host = "svm-lw4u16-comp6235-group-6.ecs.soton.ac.uk"
+    port = 27018
+    auth = True
+    db = "restaurants"
     client = None
 
     def __init__(self):
@@ -25,7 +23,7 @@ class DbConnection:
     def get_restaurant_db(self):
         db = self.client[self.db]
         if self.auth:
-            db.authenticate('user', 'banana4');
+            db.authenticate('user', 'banana4')
         return db
 
     def get_yelp_collection(self, db=None):
